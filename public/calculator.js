ecfe435052f7d96ddd6bbb38d30c147534cc19f7
Nicole Li <hla110@sfu.ca>
2022-01-29 16:45:56 -0800

    implemented percent calculation and two buttons for weighted and mean result

--- public/calculator.js
+++ public/calculator.js
@@ -1,6 +1,22 @@
 var tbl = document.getElementById("tbl");
 
+//function to calculate percentage of each activity
+function calculatePercent(event) {
+    var actualGrade = event.srcElement.parentNode.childNodes[1].value;
+    var maxGrade = event.srcElement.parentNode.childNodes[3].value;
+    var percent = actualGrade/maxGrade;
+    var percentCell = event.srcElement.parentNode.parentNode.childNodes[9];
+    percentCell.innerHTML = (percent*100).toFixed(2);
+}
 
+//set EventListener for grade entries
+function setEventListener(){
+    var myGradeList = document.getElementsByClassName("grade");
+    for (var i = 0; i < myGradeList.length; i++) {
+        myGradeList[i].onchange = calculatePercent;
+    }
+}
+setEventListener();
 
 //function to add rows to the activity table
 function addActivity()
@@ -11,37 +27,45 @@ function addActivity()
     tr.cells[1].innerHTML = "A" + currentIndex;
     tr.removeAttribute("style");
     tbl.appendChild(tr);
-}
-   
-    
-//function to calculate percentage of each activity
-document.getElementsByClassName("actualGrade").onchange = function (x) {
-    log.document.
-    ng-model
-    // var actualGrade = x.paren.getElementsByName("actualGrade");
-    // var maxGrade = document.getElementsByName("maxGrade");
-    // var percent = actualGrade/maxGrade;
-    // percent.addEventListener("percent", calculatePercent)
-    
-    // var tr = document.getElementById("sampleRow");
-    // tr.cells[4].innerHTML = percent;
-    // log.textContent = percent.target.value;
-    
-
-}
-//get listener for the grade
-//"Percent" cell onchange of "grade"
-//display percent under the "Percent" cell
-
-
+    setEventListener();
+};
 
 //function to calculate weighted result
 function calculateWeighted() {
-    
+    console.log(1);
+    var weightList = document.getElementsByClassName("weight");
+    var percentList = document.getElementsByClassName("percent");
+    var sum = 0;
+    var weightedResult = 0;
+    var weightCount = 0;
+    for (var i = 0; i < percentList.length; i++) {
+        if (percentList[i].innerHTML != null && percentList[i].innerHTML !='') {
+            if(weightList[i].value == null || weightList[i].value == ''){
+                weightList[i].value = 1;
+                sum += (parseFloat(percentList[i].innerHTML) * parseFloat(weightList[i].value));
+                weightCount += parseFloat(weightList[i].value);
+            } else {
+                sum += (parseFloat(percentList[i].innerHTML) * parseFloat(weightList[i].value));
+                weightCount += parseFloat(weightList[i].value);
+            }
+        }
+    }
+    weightedResult = sum / weightCount;
+    document.getElementById("result").innerHTML = weightedResult.toFixed(3);
 }
 
 //funciton to calculate mean result
 function calculateMean() {
-    var result = getElementById("result");
-    result = 10;
+    var percentList = document.getElementsByClassName("percent");
+    var sum = 0;
+    var mean = 0;
+    var count = 0;
+    for (var i = 0; i < percentList.length; i++) {
+        if (percentList[i].innerHTML != null && percentList[i].innerHTML !='') {
+            sum += parseFloat(percentList[i].innerHTML);
+            count++;
+        }
+    }
+    mean = sum / count;
+    document.getElementById("result").innerHTML = mean.toFixed(3);
 }
